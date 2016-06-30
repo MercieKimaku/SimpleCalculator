e3d207be539c0200e17a595066f7b2b2d92fa3af
samkomo <ilabadmin@ilabadmins-iMac-14.local>
2016-06-30 11:09:24 +0300

    Initial commit

--- SimpleCalculator/ViewController.h
+++ SimpleCalculator/ViewController.h
@@ -8,7 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum{ Plus,Minus,Multiply,Divide} CalcOperation;
+NSString *storage;
+CalcOperation operation;
+
 @interface ViewController : UIViewController
+
+
+
+- (IBAction)btnSeven:(UIButton *)sender;
+- (IBAction)btnEight:(UIButton *)sender;
+- (IBAction)btnNine:(UIButton *)sender;
+- (IBAction)btnClear:(UIButton *)sender;
+- (IBAction)btnFour:(UIButton *)sender;
+- (IBAction)btnFive:(UIButton *)sender;
+- (IBAction)btnSix:(UIButton *)sender;
+- (IBAction)btnOne:(UIButton *)sender;
+- (IBAction)btnTwo:(UIButton *)sender;
+- (IBAction)btnThree:(UIButton *)sender;
+- (IBAction)btnZero:(UIButton *)sender;
+- (IBAction)btnAdd:(UIButton *)sender;
+- (IBAction)btnSub:(UIButton *)sender;
+- (IBAction)btnDivide:(UIButton *)sender;
+- (IBAction)btnMultiply:(UIButton *)sender;
+- (IBAction)btnEqual:(UIButton *)sender;
+@property (strong, nonatomic) IBOutlet UITextField *txtView;
+
 
 
 @end
--- SimpleCalculator/ViewController.m
+++ SimpleCalculator/ViewController.m
@@ -24,4 +24,61 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)btnSeven:(UIButton *)sender {
+    _txtView.text =[NSString stringWithFormat:@"%@7", _txtView.text];
+}
+- (IBAction)btnEight:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@8", _txtView.text];
+}
+
+- (IBAction)btnNine:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@9", _txtView.text];
+}
+
+- (IBAction)btnClear:(UIButton *)sender {
+}
+
+- (IBAction)btnFour:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@4", _txtView.text];
+}
+
+- (IBAction)btnFive:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@5", _txtView.text];
+}
+
+- (IBAction)btnSix:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@6", _txtView.text];
+}
+
+- (IBAction)btnOne:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@1", _txtView.text];
+}
+
+- (IBAction)btnTwo:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@2", _txtView.text];
+}
+
+- (IBAction)btnThree:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@3", _txtView.text];}
+
+- (IBAction)btnZero:(UIButton *)sender {
+    _txtView.text = [NSString stringWithFormat:@"%@0", _txtView.text];}
+
+- (IBAction)btnAdd:(UIButton *)sender {
+}
+
+- (IBAction)btnSub:(UIButton *)sender {
+}
+
+- (IBAction)btnDivide:(UIButton *)sender {
+}
+
+- (IBAction)btnMultiply:(UIButton *)sender {
+}
+
+- (IBAction)btnEqual:(UIButton *)sender {
+}
+
+
 @end
